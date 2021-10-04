af7efdcae4540c3bdae108e866227462ac904647
Mingqi948 <yangmingqi1001@sina.com>
2021-10-04 21:12:45 +0100

    Delete quote-ws-all project

--- quote-ws-all/client/src/main/java/quote/StockQuoteService.java
+++
@@ -1,9 +0,0 @@
-package quote;
-
-import javax.jws.WebMethod;
-import javax.jws.WebService;
-
-@WebService
-public interface StockQuoteService {
-    @WebMethod public double getStockPrice(String name);
-}
