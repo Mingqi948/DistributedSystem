666c29e6b46a0fdacc23d013b7c5b36c8f91b307
Mingqi948 <yangmingqi1001@sina.com>
2021-11-14 01:30:36 +0000

    Remove calculator-master

--- calculator-master/core/src/main/java/Calculator.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Calculator extends Remote {
-    long add(long a, long b) throws RemoteException;
-    long sub(long a, long b) throws RemoteException;
-    long mul(long a, long b) throws RemoteException;
-    long div(long a, long b) throws RemoteException;
-}
--- calculator-master/server/src/main/java/CalculatorImpl.java
+++
@@ -1,6 +0,0 @@
-public class CalculatorImpl implements Calculator {
-    public long add(long a, long b) { return a+b; }
-    public long sub(long a, long b) { return a-b; }
-    public long mul(long a, long b) { return a*b; };
-    public long div(long a, long b) { return a/b; };
-}
